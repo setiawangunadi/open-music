715046743c03de5151896c5f9eb67f47988b48b5
sgudprod <sgudproduction@gmail.com>
2023-05-15 19:52:03 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -10,9 +10,14 @@ const songs = require('./api/song');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
     const albumsService = new AlbumsService();
     const songsService = new SongsService();
+    const usersService = new UsersService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -38,6 +43,13 @@ const init = async () => {
                 service: songsService,
                 validator: SongsValidator,
             }
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
         }
     ]);
 
